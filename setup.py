439a71f2bd6aac94e40a55a86dcf1a4dfb833390
Ali Can Gönüllü <alicangonullu@yahoo.com>
2023-04-24 12:39:39 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="bingpython",
-    version="0.0.19",
+    version="0.0.20",
     license="GNU General Public License v2.0",
     author="Ali Can Gönüllü",
     author_email="alicangonullu@yahoo.com",
